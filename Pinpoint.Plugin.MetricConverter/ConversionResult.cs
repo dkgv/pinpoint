1df3c2efd4578975470a48de66fd9ecd4a8260a4
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2020-10-14 12:46:36 +0200

    Revert "Revert "Implement distance conversion plugin""
    This reverts commit fae58d650c1ee41511a4e3e7f6838181f4cf73ae.

---
+++ Pinpoint.Plugin.MetricConverter/ConversionResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Pinpoint.Plugin.MetricConverter
+{
+    public class ConversionResult : IQueryResult
+    {
+        public ConversionResult(string expression, string result)
+        {
+            Title = " = " + result;
+        }
+        public string Title { get; }
+        
+        public string Subtitle { get; }
+        
+        public object Instance { get; }
+        public void OnSelect()
+        {
+        }
+    }
+}
