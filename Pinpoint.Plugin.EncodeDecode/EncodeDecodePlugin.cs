b2c552bd1188edb8c568c18c44012b517c79d54f
anderskloborg <anderskloborg@gmail.com>
2021-01-27 00:57:04 +0100

    Binary working as well, base64 still wip

--- Pinpoint.Plugin.EncodeDecode/EncodeDecodePlugin.cs
+++ Pinpoint.Plugin.EncodeDecode/EncodeDecodePlugin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -48,6 +50,10 @@ namespace Pinpoint.Plugin.EncodeDecode
             {
                 case "hex:":
                     return new HexHandler();
+                case "bin:":
+                    return new BinaryHandler();
+                case "b64:":
+                    return new Base64Handler();
                 default:
                     return new HexHandler();
             }
@@ -81,6 +87,85 @@ namespace Pinpoint.Plugin.EncodeDecode
             }
 
             return Encoding.ASCII.GetString(bytes);
+        }
+    }
+
+    public class Base64Handler: IEncodeDecodeHandler
+    {
+        private readonly Regex _decodeRegex = new Regex(@"[^-A-Za-z0-9+/=]|=[^=]|={3,}$");
+
+        public string Encode(string stringToEncode)
+        {
+            var bytes = Encoding.UTF8.GetBytes(stringToEncode);
+            return Convert.ToBase64String(bytes);
+        }
+
+        public string Decode(string stringToDecode)
+        {
+            if (!_decodeRegex.IsMatch(stringToDecode)) return "Could not decode as hex";
+
+            var bytes = Convert.FromBase64String(stringToDecode);
+
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+
+    public class BinaryHandler: IEncodeDecodeHandler
+    {
+        private readonly Regex _encodeRegex = new Regex(@"^[0-9]+$");
+        private readonly Regex _decodeRegex = new Regex(@"^[0-1]+$");
+
+        //Blatantly stolen from this answer: https://stackoverflow.com/a/15447131/7230001
+        public string Encode(string number)
+        {
+            if (!_encodeRegex.IsMatch(number)) return "Could not encode as binary";
+
+            var bigInteger = BigInteger.Parse(number);
+
+            var bytes = bigInteger.ToByteArray();
+            var idx = bytes.Length - 1;
+
+            // Create a StringBuilder having appropriate capacity.
+            var base2 = new StringBuilder(bytes.Length * 8);
+
+            // Convert first byte to binary.
+            var binary = Convert.ToString(bytes[idx], 2);
+
+            // Ensure leading zero exists if value is positive.
+            if (binary[0] != '0' && bigInteger.Sign == 1)
+            {
+                base2.Append('0');
+            }
+
+            // Append binary string to StringBuilder.
+            base2.Append(binary);
+
+            // Convert remaining bytes adding leading zeros.
+            for (idx--; idx >= 0; idx--)
+            {
+                base2.Append(Convert.ToString(bytes[idx], 2).PadLeft(8, '0'));
+            }
+
+            return base2.ToString();
+        }
+
+        public string Decode(string binaryString)
+        {
+            if (!_decodeRegex.IsMatch(binaryString)) return "Could not decode as binary";
+
+            BigInteger one = 1;
+            BigInteger result = 0;
+
+            foreach (var c in binaryString)
+            {
+                result <<= 1;
+                if (c == '1')
+                {
+                    result |= one;
+                }
+            }
+
+            return result.ToString();
         }
     }
 
