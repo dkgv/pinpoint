309278d0575f1cc05189e3aeae9f939bdf084a81
Gustav <gustav-git@pm.me>
2021-02-05 15:49:10 +0100

    Fix potential crash

--- Pinpoint.Plugin.Currency/CurrencyPlugin.cs
+++ Pinpoint.Plugin.Currency/CurrencyPlugin.cs
@@ -14,9 +14,9 @@ namespace Pinpoint.Plugin.Currency
 {
     public class CurrencyPlugin : IPlugin
     {
+        private CurrencyRepository _currencyRepo;
         private string _baseCurrency;
         private const string Symbols = "$£€¥";
-        private static readonly Dictionary<string, CurrencyModel> CurrencyModels = new Dictionary<string, CurrencyModel>();
 
         public PluginMeta Meta { get; set; } = new PluginMeta("Currency Converter", PluginPriority.Highest);
 
@@ -32,80 +32,7 @@ namespace Pinpoint.Plugin.Currency
             {
                 _baseCurrency = "USD";
             }
-
-            CacheFiatRates(_baseCurrency);
-            CacheCryptoRates();
-        }
-
-        private void CacheFiatRates(string from)
-        {
-            // Load exchange rates
-            var url = $"https://api.exchangeratesapi.io/latest?base={from}";
-            try
-            {
-                using var client = new WebClient();
-                var json = client.DownloadString(url);
-                CurrencyModels[from] = JsonConvert.DeserializeObject<CurrencyModel>(json);
-            }
-            catch (WebException)
-            {
-                // Likely invalid currency
-            }
-        }
-
-        private void CacheCryptoRates()
-        {
-            // Ensure USD rates are cached
-            CacheFiatRates("USD");
-
-            CurrencyModel PopulateModel(string ticker, double usdPrice)
-            {
-                var model = new CurrencyModel(ticker)
-                {
-                    Rates = new Dictionary<string, dynamic> {["USD"] = usdPrice}
-                };
-
-                foreach (var @base in CurrencyModels["USD"].Rates.Keys.Where(@base => !@base.Equals("USD")))
-                {
-                    model.Rates[@base] = ConvertFromTo("USD", usdPrice, @base);
-                }
-
-                return model;
-            }
-            
-            const string url = "https://coinmarketcap.com/";
-            var doc = new HtmlWeb().Load(url);
-            
-            const string xpath = "/html/body/div[1]/div/div[2]/div/div/div[2]/table";
-            var table = doc.DocumentNode.SelectSingleNode(xpath);
-            
-            var rows = table.Descendants("tr").ToArray();
-            for (var i = 1; i < rows.Length; i++)
-            {
-                var cols = rows[i].Descendants("td").ToArray();
-                if (cols.Length != 11)
-                {
-                    continue;
-                }
-                
-                var ps = cols[2].Descendants("p").ToArray();
-                if (ps.Length != 2)
-                {
-                    continue;
-                }
-                
-                // Extract current price for entry
-                var priceNodeText = cols[3].InnerText;
-                var priceText = priceNodeText.TrimStart('$').Replace(",", "");
-                var price = double.Parse(priceText, NumberStyles.Any, CultureInfo.InvariantCulture);
-
-                // Skip stable-coins like USDT
-                if (Math.Abs(price - 1) > 0.1)
-                {
-                    var ticker = cols[2].Descendants("p").ToArray()[1].InnerText;
-                    CurrencyModels[ticker] = PopulateModel(ticker, price);
-                }
-            }
+            _currencyRepo = new CurrencyRepository(_baseCurrency);
         }
 
         public void Unload()
@@ -122,9 +49,9 @@ namespace Pinpoint.Plugin.Currency
                 return char.IsDigit(ch) || ch == '.';
             }
 
-            // Matches inputs like $100 or €5
+            // Matches inputs like $100, €5
             var hasSymbol = raw.Length > 1 && Symbols.Contains(query.Prefix());
-            if (hasSymbol && raw.Substring(1).All(IsNumber))
+            if (hasSymbol && raw[..1].All(IsNumber))
             {
                 return true;
             }
@@ -154,40 +81,15 @@ namespace Pinpoint.Plugin.Currency
             var from = IdentifyFrom(query);
             var value = IdentifyValue(query);
             var to = IdentifyTo(query);
-            var conversion = Math.Round(ConvertFromTo(from, value, to), 5);
+
+            if (string.IsNullOrEmpty(from) || string.IsNullOrEmpty(to))
+            {
+                yield break;
+            }
+
+            var conversion = Math.Round(_currencyRepo.ConvertFromTo(from, value, to), 5);
 
             yield return new CurrencyResult(conversion, to);
-        }
-
-        private double ConvertFromTo(string from, double value, string to)
-        {
-            from = from.ToUpper();
-            to = to.ToUpper();
-            
-            // Is the `from` currency new?
-            if (!CurrencyModels.ContainsKey(from))
-            {
-                CacheFiatRates(from);
-            }
-
-            var fromModel = CurrencyModels[from];
-            
-            // Check if we already know rate for `to`
-            if (fromModel.Rates.ContainsKey(to))
-            {
-                return fromModel.Rates[to] * value;
-            }
-
-            // Check if we know inverse rate
-            CurrencyModel toModel;
-            if (CurrencyModels.ContainsKey(to) && (toModel = CurrencyModels[to]).Rates.ContainsKey(from))
-            {
-                fromModel.Rates[to] = 1 / toModel.Rates[from];
-            }
-
-            return fromModel.Rates.ContainsKey(to) 
-                ? fromModel.Rates[to] * value 
-                : 1;
         }
 
         private double IdentifyValue(Query query)
@@ -213,17 +115,21 @@ namespace Pinpoint.Plugin.Currency
             }
 
             // Handles 100 usd
-            return query.Parts[1];
+            var iso = query.Parts[1];
+            return _currencyRepo.IsIsoValid(iso) ? iso : string.Empty;
         }
 
         private string IdentifyTo(Query query)
         {
+            // Handles queries like 100 usd, 100 eur
             if (!query.RawQuery.Contains("in") && !query.RawQuery.Contains("to"))
             {
                 return _baseCurrency;
             }
 
-            return SymbolToISO(query.Parts[^1]);
+            // Get last part
+            var iso = SymbolToISO(query.Parts[^1]);
+            return _currencyRepo.IsIsoValid(iso) ? iso : string.Empty;
         }
 
         private string SymbolToISO(string symbol)
