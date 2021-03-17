694808103a7c4d0aa0a16ff7b4aa1a601fc13564
Gustav <gustav-git@pm.me>
2021-03-17 13:23:35 +0100

    Change activation keyword

--- Pinpoint.Plugin.Notes/NotesPlugin.cs
+++ Pinpoint.Plugin.Notes/NotesPlugin.cs
@@ -8,6 +8,10 @@ namespace Pinpoint.Plugin.Notes
 {
     public class NotesPlugin : IPlugin
     {
+        private static readonly string[] Actions = {
+            "notes-add",
+            "n+"
+        };
         private readonly NotesManager _notesManager;
 
         public NotesPlugin()
@@ -15,29 +19,34 @@ namespace Pinpoint.Plugin.Notes
             _notesManager = NotesManager.GetInstance();
         }
 
-        public PluginMeta Meta { get; set; } = new PluginMeta("Notes plugin", PluginPriority.Highest);
+        public PluginMeta Meta { get; set; } = new PluginMeta("Notes Plugin", PluginPriority.Highest);
         
         public bool TryLoad() => true;
 
         public void Unload() { }
 
-        public Task<bool> Activate(Query query)
+        public async Task<bool> Activate(Query query)
         {
-            return Task.FromResult(query.RawQuery.StartsWith("notes"));
+            return Actions.Contains(query.Parts[0]);
         }
 
         public async IAsyncEnumerable<AbstractQueryResult> Process(Query query)
         {
             var notes = await _notesManager.GetNotes();
+            var prefix = query.Parts[0];
 
-            if (query.Parts.Length > 0 && query.Parts[0] == "notes+" || query.Parts[0] == "notes-add")
+            switch (prefix)
             {
-                var noteContent = query.RawQuery.Replace(query.Parts[0], "").TrimStart();
+                case "n+":
+                case "notes-add":
+                    var noteContent = query.RawQuery.Replace(prefix, "").TrimStart();
 
-                if (!string.IsNullOrWhiteSpace(noteContent))
-                {
-                    yield return new AddNoteResult(noteContent);
-                }
+                    if (!string.IsNullOrWhiteSpace(noteContent))
+                    {
+                        yield return new AddNoteResult(noteContent);
+                    }
+
+                    break;
             }
 
             foreach (var note in notes.OrderByDescending(n => n.CreatedAt))
