53a4159c195cd4282c00efdc5058760396e2d692
clawcastle <anderskloborg@gmail.com>
2021-03-18 11:27:00 +0100

    URL launcher plugin (#77)

---
+++ Pinpoint.Plugin.UrlLauncher/UrlLauncherResult.cs
@@ -0,0 +1,22 @@
+﻿using FontAwesome5;
+using Pinpoint.Core;
+using Pinpoint.Core.Results;
+
+namespace Pinpoint.Plugin.UrlLauncher
+{
+    public class UrlLauncherResult: AbstractFontAwesomeQueryResult
+    {
+        private readonly string _url;
+
+        public UrlLauncherResult(string url): base($"Launch {url}")
+        {
+            _url = url;
+        }
+        public override void OnSelect()
+        {
+            ProcessHelper.OpenUrl(_url);
+        }
+
+        public override EFontAwesomeIcon FontAwesomeIcon => EFontAwesomeIcon.Solid_ExternalLinkAlt;
+    }
+}
