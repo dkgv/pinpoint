0d97af3c076396c279a7ab421aa3fe086c46fca8
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2021-10-10 17:17:00 +0200

    Now handles conversion with DateTime and TimeZoneInfo.
    Uses built in classes and methods for handling the conversion instead of using an API.

--- Pinpoint.Plugin.Timezone/TimezoneConverterPlugin.cs
+++ Pinpoint.Plugin.Timezone/TimezoneConverterPlugin.cs
@@ -13,14 +13,26 @@ namespace Pinpoint.Plugin.Timezone
 {
     public class TimezoneConverterPlugin : IPlugin
     {
-        private const string Description = "Convert time to/from different timezones.\n\nExamples: \"time in PST\", \"12:00 am GMT to PST\", \"13:24 CET to JST\"";
+        private const string Description = "Convert time to/from different timezones.\n\nExamples: \"time in PST\", \"12:01 to EST\", \"12:00 am GMT to PST\", \"13:24 CET to JST\"";
 
-        private const string TwelveHourTime = @"(1[0-2]|0?[1-9]):?[0-5][0-9] (AM|am|PM|pm)";
-        private const string TwentyFourHourTime = @"([01]\d|2[0-3]):?([0-5]\d)";
-        private const string Timezone = "GMT|PST|EST|CEST|CET|JST";
+        private const string TwelveHourTime = @"(1[0-2]|0?[1-9]):[0-5][0-9] (AM|am|PM|pm)";
+        private const string TwentyFourHourTime = @"([01]\d|2[0-3]):([0-5]\d)";
+
+        private static readonly Dictionary<string, string> TimezoneIdConversion = new()
+        {
+            { "GMT", "Greenwich Standard Time" },
+            { "PST", "Pacific Standard Time" },
+            { "EST", "Eastern Standard Time" },
+            { "CEST", "Central European Standard Time" },
+            { "CET", "Central European Standard Time" },
+            { "JST", "Tokyo Standard Time" },
+        };
+
+        private static readonly string Timezone = string.Join("|", TimezoneIdConversion.Keys);
 
         private static readonly Regex Pattern =
-            new($"((time)|({TwelveHourTime} ({Timezone}))|({TwentyFourHourTime} ({Timezone}))) (to|in) ({Timezone})");
+            new($"^(time|({TwentyFourHourTime}) ?({Timezone})?|({TwelveHourTime}) ?({Timezone})?) (to|in) ({Timezone})$");
+
         public PluginMeta Meta { get; set; } = new("Timezone Converter", Description, PluginPriority.Highest);
         public PluginSettings UserSettings { get; set; } = new();
         public async Task<bool> Activate(Query query)
@@ -34,64 +46,39 @@ namespace Pinpoint.Plugin.Timezone
             if (query.RawQuery.Contains("time"))
             {
                 var targetTimezone = ConvertTimezoneCode(splitQuery[2]);
-                var url = $"https://www.timeapi.io/api/Time/current/zone?timeZone={targetTimezone}";
+                var time = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById(targetTimezone));
 
-                var time = await HttpHelper.SendGet(url, JObject.Parse);
-                yield return new TimezoneConversionResult(time["time"].ToString());
+                yield return new TimezoneConversionResult(time.ToShortTimeString());
             }
             else
             {
-                var queryTime = $"{splitQuery[0]}:00";
+                // Bit of a mess - handling all the different types of input strings.
+                var queryTime = splitQuery[0];
                 var sourceTimezone = ConvertTimezoneCode(splitQuery[1]);
-                var targetTimezone = ConvertTimezoneCode(splitQuery[3]);
+                var targetTimezone = ConvertTimezoneCode(splitQuery[^1]);
                 
                 var isOnTwelveHourForm = 
                     splitQuery[1] == "am" || splitQuery[1] == "pm" ||
                     splitQuery[1] == "AM" || splitQuery[1] == "PM";
-
+                
                 if (isOnTwelveHourForm)
                 {
-                    queryTime = ConvertToMilitaryTime($"{splitQuery[0]} {splitQuery[1]}");
+                    queryTime = $"{splitQuery[0]} {splitQuery[1]}";
                     sourceTimezone = ConvertTimezoneCode(splitQuery[2]);
-                    targetTimezone = ConvertTimezoneCode(splitQuery[4]);
                 }
-                
-                const string url = "https://www.timeapi.io/api/Conversion/ConvertTimeZone";
-                var jsonQuery = new
-                {
-                    fromTimezone = sourceTimezone,
-                    dateTime = $"{DateTime.Today:yyyy-MM-dd} {queryTime}",
-                    toTimezone = targetTimezone,
-                    dstAmbiguity = ""
-                };
-                
-                var time = await HttpHelper.SendPost(url, JsonContent.Create(jsonQuery), 
-                     message => JObject.Parse(message.Content.ReadAsStringAsync(ct).Result));
-                
-                var returnTime = time["conversionResult"]["time"].ToString();
-                var formattedTime = isOnTwelveHourForm ? DateTime.Parse(returnTime).ToString("h:mm tt") : returnTime;
 
+                var time = TimeZoneInfo.ConvertTime(DateTime.Parse(queryTime),
+                    TimeZoneInfo.FindSystemTimeZoneById(sourceTimezone), 
+                    TimeZoneInfo.FindSystemTimeZoneById(targetTimezone));
+                
+                var formattedTime = isOnTwelveHourForm ? time.ToString("h:mm tt") : time.ToShortTimeString();
                 yield return new TimezoneConversionResult(formattedTime);
             }
         }
 
         private static string ConvertTimezoneCode(string code)
         {
-            return code switch // TODO: Add more (all) timezones.
-            {
-                "GMT" => "Etc/Greenwich",
-                "PST" => "America/Los_Angeles",
-                "EST" => "America/New_York",
-                "CEST" => "Europe/Amsterdam",
-                "CET" => "Europe/Amsterdam",
-                "JST" => "Asia/Tokyo",
-                _ => "Europe/Amsterdam"
-            };
-        }
-
-        private static string ConvertToMilitaryTime(string time)
-        {
-            return DateTime.Parse(time).ToString("HH:mm:ss");
+            return TimezoneIdConversion.TryGetValue(code, out code) ? code : TimeZoneInfo.Local.Id;
         }
     }
 }
