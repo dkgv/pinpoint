fb4272a5347d2c326f876c1b762f14761930ec1b
Gustav <gustav-git@pm.me>
2021-10-21 17:24:16 +0200

    Allow for not specifying minutes

--- Pinpoint.Plugin.Timezone/TimezoneConverterPlugin.cs
+++ Pinpoint.Plugin.Timezone/TimezoneConverterPlugin.cs
@@ -14,7 +14,7 @@ namespace Pinpoint.Plugin.Timezone
     {
         private const string Description = "Convert time to/from different timezones.\n\nExamples: \"time in PST\", \"12:01 to EST\", \"12:00 am GMT to PST\", \"13:24 CET to JST\"";
 
-        private const string TwelveHourTime = @"(1[0-2]|0?[1-9]):[0-5][0-9] (AM|am|PM|pm)";
+        private const string TwelveHourTime = @"(1[0-2]|0?[1-9])(:[0-5][0-9])? (AM|am|PM|pm)";
         private const string TwentyFourHourTime = @"([01]\d|2[0-3]):([0-5]\d)";
 
         private static readonly Dictionary<string, string> TimezoneIdConversion = new()
