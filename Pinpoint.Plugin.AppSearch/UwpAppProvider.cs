da43f3ce0e0264029073e875eb13ec7ce9d436df
Gustav <gustav-git@pm.me>
2022-02-13 19:27:22 +0100

    Simplify and move `PowerShellToJson` to helper class

--- Pinpoint.Plugin.AppSearch/UwpAppProvider.cs
+++ Pinpoint.Plugin.AppSearch/UwpAppProvider.cs
@@ -1,119 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
+using Pinpoint.Core;
 
 namespace Pinpoint.Plugin.AppSearch
 {
     public class UwpAppProvider : IAppProvider
     {
+        private const string FindAppsCommand =
+            @"Get-StartApps | Where-Object { $_.AppID.Contains('!') }";
+        private const string FindIconsCommand =
+            "Get-AppxPackage -PackageTypeFilter Main | ForEach-Object -Process { $manifest = Get-AppxPackageManifest $_.PackageFullName; $app = $manifest.Package.Applications.Application; $iconLocation = $app.VisualElements.Square44x44Logo | Select-Object -First 1; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Square30x30Logo}; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Logo}; return [PSCustomObject]@{PackageFamilyName = $_.PackageFamilyName; InstallLocation = $_.InstallLocation; IconLocation = $iconLocation} }";
+
         public IEnumerable<IApp> Provide()
         {
-            const string args = @"Get-StartApps | Where-Object { $_.AppID.Contains('!') } | ConvertTo-Json";
-            var apps = RunProcess<List<UwpApp>>(args);
-
-            FetchAndSetAppIcons(apps);
-
+            var apps = ProcessHelper.PowerShellToJson<List<UwpApp>>(FindAppsCommand);
+            FetchAndAddAppIcons(apps);
             return apps;
         }
 
-        private static void FetchAndSetAppIcons(List<UwpApp> apps)
+        private static void FetchAndAddAppIcons(List<UwpApp> apps)
         {
-            const string args =
-                "Get-AppxPackage -PackageTypeFilter Main | ForEach-Object -Process { $manifest = Get-AppxPackageManifest $_.PackageFullName; $app = $manifest.Package.Applications.Application; $iconLocation = $app.VisualElements.Square44x44Logo | Select-Object -First 1; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Square30x30Logo}; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Logo}; return [PSCustomObject]@{PackageFamilyName = $_.PackageFamilyName; InstallLocation = $_.InstallLocation; IconLocation = $iconLocation} } | ConvertTo-Json";
-            var appxPackages = RunProcess<List<AppxPackage>>(args);
+            var appxPackages = ProcessHelper.PowerShellToJson<List<AppxPackage>>(FindIconsCommand);
             foreach (var app in apps)
             {
                 // Look through only the apps that come from Get-StartApps
                 var appFamilyName = app.FilePath[..app.FilePath.IndexOf("!")];
-                foreach (var package in appxPackages.Where(package => appFamilyName == package.PackageFamilyName))
+                var package = appxPackages.FirstOrDefault(package => appFamilyName == package.PackageFamilyName);
+                if (package == default)
                 {
-                    // Separate the folder and the icon name
-                    var location = package.IconLocation as string;
-                    var idxOfLastDir = location.LastIndexOf("\\");
-                    var iconName = location[(idxOfLastDir + 1)..];
-                    var iconLocation = location[..idxOfLastDir];
-
-                    iconLocation = package.InstallLocation + "\\" + iconLocation;
-                    app.IconLocation = FindCorrectIconPath(iconLocation, iconName[..iconName.IndexOf(".")]);
+                    continue;
                 }
+
+                // Separate the folder and the icon name
+                var location = package.IconLocation as string;
+                var idxOfLastDir = location.LastIndexOf("\\");
+                var iconName = location[(idxOfLastDir + 1)..];
+                var iconLocation = location[..idxOfLastDir];
+
+                iconLocation = package.InstallLocation + "\\" + iconLocation;
+                app.IconLocation = FindCorrectIconPath(iconLocation, iconName[..iconName.IndexOf(".")]);
             }
-        }
-
-        private static T RunProcess<T>(string args)
-        {
-            var process = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "powershell",
-                    Arguments = args,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }
-            };
-            process.Start();
-
-            var sb = new StringBuilder();
-            while (!process.StandardOutput.EndOfStream)
-            {
-                sb.Append(process.StandardOutput.ReadLine());
-            }
-
-            return JsonConvert.DeserializeObject<T>(sb.ToString());
         }
 
         private static string FindCorrectIconPath(string iconLocation, string name)
         {
-            if (!Directory.Exists(iconLocation)) return "";
+            if (!Directory.Exists(iconLocation))
+            {
+                return "";
+            }
 
             var files = Directory.GetFiles(iconLocation, $"{name}.*", SearchOption.TopDirectoryOnly);
-            foreach (var file in files)
+            foreach (var file in files.Where(f => f.Contains(".png")))
             {
-                if (!file.Contains(".png")) continue;
-
                 // Look for correct option of icon - scale between 150-800
                 if (file.Contains(".scale-"))
                 {
-                    if (Regex.IsMatch(file, @".*\.scale-(800|400|200|150)\.png"))
+                    if (!Regex.IsMatch(file, @".*\.scale-(800|400|200|150)\.png"))
                     {
-                        return file;
+                        continue;
                     }
-                    continue;
-                }
 
-                // Look for correct option of icon - targetsize >= 48
-                if (file.Contains(".targetsize-"))
-                {
-                    if (Regex.IsMatch(file, @".*\.targetsize-((48)|(49)|(([5-9]\d|[1-9]\d{2,})))\.png"))
-                    {
-                        return file;
-                    }
-                    continue;
+                    return file;
                 }
 
                 // If file is not named with scale or targetsize
+                if (!file.Contains(".targetsize-"))
+                {
+                    return file;
+                }
+
+                // Look for correct option of icon - targetsize >= 48
+                if (!Regex.IsMatch(file, @".*\.targetsize-((48)|(49)|(([5-9]\d|[1-9]\d{2,})))\.png"))
+                {
+                    continue;
+                }
+
                 return file;
             }
+
             return "";
         }
-    }
-
-    [Serializable]
-    internal struct AppxPackage
-    {
-        [JsonProperty("PackageFamilyName")]
-        public string PackageFamilyName { get; set; }
-
-        [JsonProperty("InstallLocation")]
-        public string InstallLocation { get; set; }
-
-        [JsonProperty("IconLocation")]
-        public object IconLocation { get; set; }
+        record AppxPackage([JsonProperty("PackageFamilyName")] string PackageFamilyName, [JsonProperty("InstallLocation")] string InstallLocation, [JsonProperty("IconLocation")] object IconLocation);
     }
 }
