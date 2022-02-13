fc5692d4be653ccb811ec83dbb7a579317d7f0cb
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2022-02-13 18:34:14 +0100

    added comments

--- Pinpoint.Plugin.AppSearch/UwpAppProvider.cs
+++ Pinpoint.Plugin.AppSearch/UwpAppProvider.cs
@@ -21,6 +21,29 @@ namespace Pinpoint.Plugin.AppSearch
             return apps;
         }
 
+        private static void FetchAndSetAppIcons(List<UwpApp> apps)
+        {
+            const string args =
+                "Get-AppxPackage -PackageTypeFilter Main | ForEach-Object -Process { $manifest = Get-AppxPackageManifest $_.PackageFullName; $app = $manifest.Package.Applications.Application; $iconLocation = $app.VisualElements.Square44x44Logo | Select-Object -First 1; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Square30x30Logo}; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Logo}; return [PSCustomObject]@{PackageFamilyName = $_.PackageFamilyName; InstallLocation = $_.InstallLocation; IconLocation = $iconLocation} } | ConvertTo-Json";
+            var appxPackages = RunProcess<List<AppxPackage>>(args);
+            foreach (var app in apps)
+            {
+                // Look through only the apps that come from Get-StartApps
+                var appFamilyName = app.FilePath[..app.FilePath.IndexOf("!")];
+                foreach (var package in appxPackages.Where(package => appFamilyName == package.PackageFamilyName))
+                {
+                    // Separate the folder and the icon name
+                    var location = package.IconLocation as string;
+                    var idxOfLastDir = location.LastIndexOf("\\");
+                    var iconName = location[(idxOfLastDir + 1)..];
+                    var iconLocation = location[..idxOfLastDir];
+
+                    iconLocation = package.InstallLocation + "\\" + iconLocation;
+                    app.IconLocation = FindCorrectIconPath(iconLocation, iconName[..iconName.IndexOf(".")]);
+                }
+            }
+        }
+
         private static T RunProcess<T>(string args)
         {
             var process = new Process
@@ -45,27 +68,6 @@ namespace Pinpoint.Plugin.AppSearch
             return JsonConvert.DeserializeObject<T>(sb.ToString());
         }
 
-        private static void FetchAndSetAppIcons(List<UwpApp> apps)
-        {
-            const string args =
-                "Get-AppxPackage -PackageTypeFilter Main | ForEach-Object -Process { $manifest = Get-AppxPackageManifest $_.PackageFullName; $app = $manifest.Package.Applications.Application; $iconLocation = $app.VisualElements.Square44x44Logo | Select-Object -First 1; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Square30x30Logo}; if ($iconLocation -eq $null) {$iconLocation = $app.VisualElements.Logo}; return [PSCustomObject]@{PackageFamilyName = $_.PackageFamilyName; InstallLocation = $_.InstallLocation; IconLocation = $iconLocation} } | ConvertTo-Json";
-            var appxPackages = RunProcess<List<AppxPackage>>(args);
-            foreach (var app in apps)
-            {
-                var appFamilyName = app.FilePath[..app.FilePath.IndexOf("!")];
-                foreach (var package in appxPackages.Where(package => appFamilyName == package.PackageFamilyName))
-                {
-                    var location = package.IconLocation as string;
-                    var idxOfLastDir = location.LastIndexOf("\\");
-                    var iconName = location[(idxOfLastDir + 1)..];
-                    var iconLocation = location[..idxOfLastDir];
-
-                    iconLocation = package.InstallLocation + "\\" + iconLocation;
-                    app.IconLocation = FindCorrectIconPath(iconLocation, iconName.Split(".").First());
-                }
-            }
-        }
-
         private static string FindCorrectIconPath(string iconLocation, string name)
         {
             if (!Directory.Exists(iconLocation)) return "";
@@ -75,6 +77,7 @@ namespace Pinpoint.Plugin.AppSearch
             {
                 if (!file.Contains(".png")) continue;
 
+                // Look for correct option of icon - scale between 150-800
                 if (file.Contains(".scale-"))
                 {
                     if (Regex.IsMatch(file, @".*\.scale-(800|400|200|150)\.png"))
@@ -84,6 +87,7 @@ namespace Pinpoint.Plugin.AppSearch
                     continue;
                 }
 
+                // Look for correct option of icon - targetsize >= 48
                 if (file.Contains(".targetsize-"))
                 {
                     if (Regex.IsMatch(file, @".*\.targetsize-((48)|(49)|(([5-9]\d|[1-9]\d{2,})))\.png"))
@@ -93,6 +97,7 @@ namespace Pinpoint.Plugin.AppSearch
                     continue;
                 }
 
+                // If file is not named with scale or targetsize
                 return file;
             }
             return "";
