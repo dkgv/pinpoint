0b1be3bf00153e07c4f521892ce578a5ec79edad
Gustav <gustav-git@pm.me>
2022-02-18 22:42:54 +0100

    Uppercase s

--- Pinpoint.Core/AppConstants.cs
+++ Pinpoint.Core/AppConstants.cs
@@ -8,7 +8,7 @@ namespace Pinpoint.Core
         private static readonly string UserHome = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
         public static readonly string MainDirectory = UserHome + Path.DirectorySeparatorChar + "Pinpoint" + Path.DirectorySeparatorChar;
-        public static readonly string SettingsFilePath = MainDirectory + "settings.json";
+        public static readonly string SettingsFilePath = MainDirectory + "Settings.json";
 
         public const string HotkeyToggleVisibilityId = "ToggleVisibility";
         public const string HotkeyPasteId = "PasteFromClipboard";
