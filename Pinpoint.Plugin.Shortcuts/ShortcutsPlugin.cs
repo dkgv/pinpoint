14d0834b0607f57267d68b595c77514a60b33712
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2022-03-17 19:49:35 +0100

    added a query feature to the plugin
    Users can now define:
    
    Name: reddit; Value: https://reddit.com/r/{query}
    
    This allows a user to write:
    reddit memes -> https://reddit.com/r/memes

--- Pinpoint.Plugin.Shortcuts/ShortcutsPlugin.cs
+++ Pinpoint.Plugin.Shortcuts/ShortcutsPlugin.cs
@@ -10,7 +10,7 @@ namespace Pinpoint.Plugin.Shortcuts
 {
     public class ShortcutsPlugin : IPlugin
     {
-        private static readonly string Description = $"Create custom shortcuts to open file locations, settings and even websites.\n\nExamples:\nName: youtube; Value: https://youtube.com,\nName: desktop; Value: C:\\Users\\{Environment.UserName}\\Desktop,\nName: apps; Value: ms-settings:appsfeatures";
+        private static readonly string Description = $"Create custom shortcuts to open file locations, settings and even websites.\n\nExamples:\nName: youtube; Value: https://youtube.com,\nName: desktop; Value: C:\\Users\\{Environment.UserName}\\Desktop,\nName: apps; Value: ms-settings:appsfeatures, \nName: desktop; Value: C:\\Users\\{Environment.UserName}\\Desktop,\nName: reddit; Value: https://www.reddit.com/r/{{query}}";
 
         public PluginMeta Meta { get; set; } = new("Shortcuts", Description, PluginPriority.Highest);
 
@@ -29,12 +29,27 @@ namespace Pinpoint.Plugin.Shortcuts
 
         public async Task<bool> Activate(Query query)
         {
-            return UserSettings.Any(setting => setting.Name.Equals(query.RawQuery));
+            if (query.Parts.Length == 2) return UserSettings.Any(setting => setting.Name.Equals(query.Parts[0]));
+            {
+                return UserSettings.Any(setting =>
+                    setting.Name.Equals(query.RawQuery) && !setting.Value.ToString()!.Contains("query"));;
+            }
         }
 
         public async IAsyncEnumerable<AbstractQueryResult> Process(Query query, CancellationToken ct)
         {
-            yield return new ShortcutsResult(query.RawQuery, UserSettings.Str(query.RawQuery));
+            ShortcutsResult result;
+            if (query.Parts.Length == 1)
+            {
+                result = new ShortcutsResult(query.RawQuery, UserSettings.Str(query.RawQuery));
+            }
+            else
+            {
+                var q = query.Parts;
+                result = new ShortcutsResult(q[0], UserSettings.Str(q[0]).Replace("{query}", q[1]));
+            }
+
+            yield return result;
         }
     }
 }
