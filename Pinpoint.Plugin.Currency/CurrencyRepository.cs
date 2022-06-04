4ef9ffed240f99704dcbb40bdb1ce258f9f4f2fe
Gustav <gustav-git@pm.me>
2022-06-04 10:27:11 +0200

    Fetch crypto rates from API instead of parsing HTML

--- Pinpoint.Plugin.Currency/CurrencyRepository.cs
+++ Pinpoint.Plugin.Currency/CurrencyRepository.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
 using Newtonsoft.Json;
 
 namespace Pinpoint.Plugin.Currency
@@ -16,10 +12,7 @@ namespace Pinpoint.Plugin.Currency
 
         public async Task LoadCurrenciesInitial()
         {
-            await CacheFiatRatesEurBase().ConfigureAwait(false);
-            await CacheCryptoRates().ConfigureAwait(false);
-
-            //await Task.WhenAll(cacheFiatRatesTask, cacheCryptoRatesTask);
+            await CacheRatesWithEurBase().ConfigureAwait(false);
 
             foreach (var (@base, model) in CurrencyModels)
             {
@@ -35,8 +28,6 @@ namespace Pinpoint.Plugin.Currency
 
         public Dictionary<string, CurrencyModel> CurrencyModels { get; } = new();
 
-        // Convert all values for USD
-        
         public double ConvertFromTo(string from, double value, string to)
         {
             from = from.ToUpper();
@@ -85,7 +76,7 @@ namespace Pinpoint.Plugin.Currency
             return 1;
         }
 
-        private async Task CacheFiatRatesEurBase()
+        private async Task CacheRatesWithEurBase()
         {
             // Load exchange rates
             var url = "https://usepinpoint.com/api/currency";
@@ -99,65 +90,6 @@ namespace Pinpoint.Plugin.Currency
             catch (WebException)
             {
                 // Likely invalid currency
-            }
-        }
-
-        private async Task CacheCryptoRates()
-        {
-            CurrencyModel PopulateModel(string ticker, double usdPrice)
-            {
-                var eurPrice = ConvertFromTo("USD", usdPrice, "EUR");
-
-                var model = new CurrencyModel(ticker)
-                {
-                    Rates = new Dictionary<string, dynamic> { ["EUR"] = eurPrice }
-                };
-
-                foreach (var @base in CurrencyModels["EUR"].Rates.Keys.Where(@base => !@base.Equals("EUR")))
-                {
-                    model.Rates[@base] = ConvertFromTo("EUR", usdPrice, @base);
-                }
-
-                return model;
-            }
-
-            var htmlWeb = new HtmlWeb();
-            const string url = "https://coinmarketcap.com/";
-            var doc = await htmlWeb.LoadFromWebAsync(url).ConfigureAwait(false);
-
-            const string xpath = "/html/body/div/div/div[1]/div[2]/div/div[1]/div[4]/table";
-            var table = doc.DocumentNode.SelectSingleNode(xpath);
-            if (table == null)
-            {
-                return;
-            }
-
-            var rows = table.Descendants("tr").ToArray();
-            for (var i = 1; i < rows.Length; i++)
-            {
-                var cols = rows[i].Descendants("td").ToArray();
-                if (cols.Length != 11)
-                {
-                    continue;
-                }
-
-                var ps = cols[2].Descendants("p").ToArray();
-                if (ps.Length != 2)
-                {
-                    continue;
-                }
-
-                // Extract current price for entry
-                var priceNodeText = cols[3].InnerText;
-                var priceText = priceNodeText.TrimStart('$').Replace(",", "");
-                var price = double.Parse(priceText, NumberStyles.Any, CultureInfo.InvariantCulture);
-
-                // Skip stable-coins like USDT
-                if (Math.Abs(price - 1) > 0.1)
-                {
-                    var ticker = cols[2].Descendants("p").ToArray()[1].InnerText;
-                    CurrencyModels[ticker] = PopulateModel(ticker, price);
-                }
             }
         }
     }
