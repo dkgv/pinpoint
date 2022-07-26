e7f89b5198adef11e9f2afb887e422f0a97b3a60
Gustav <gustav-git@pm.me>
2022-07-26 19:18:30 +0200

    Fix rebase

--- Pinpoint.Plugin.Shortcuts/ShortcutsPlugin.cs
+++ Pinpoint.Plugin.Shortcuts/ShortcutsPlugin.cs
@@ -13,27 +13,27 @@ namespace Pinpoint.Plugin.Shortcuts
         private static readonly string Description = $"Create custom shortcuts to open file locations, settings and even websites.\n\nExamples:\nName: youtube; Value: https://youtube.com,\nName: desktop; Value: C:\\Users\\{Environment.UserName}\\Desktop,\nName: apps; Value: ms-settings:appsfeatures, \nName: desktop; Value: C:\\Users\\{Environment.UserName}\\Desktop,\nName: reddit; Value: https://www.reddit.com/r/{{query}}";
 
         public PluginMeta Meta { get; set; } = new("Shortcuts", Description, PluginPriority.Highest);
+        
+        public PluginStorage Storage { get; set; } = new();
 
-        public PluginSettings UserSettings { get; set; } = new();
-
-        public bool ModifiableSettings { get; set; }
+        public bool ModifiableSettings => true;
 
         public bool IsLoaded { get; set; }
 
         public async Task<bool> TryLoad()
         {
-            ModifiableSettings = true;
-            UserSettings.Put("", "");
+            Storage.UserSettings.Put("", "");
             return IsLoaded = true;
         }
 
         public async Task<bool> Activate(Query query)
         {
-            if (query.Parts.Length == 2) return UserSettings.Any(setting => setting.Name.Equals(query.Parts[0]));
+            if (query.Parts.Length == 2)
             {
-                return UserSettings.Any(setting =>
-                    setting.Name.Equals(query.RawQuery) && !setting.Value.ToString()!.Contains("query"));;
+                return Storage.UserSettings.Any(s => s.Name.Equals(query.Parts[0]));
             }
+            
+            return Storage.UserSettings.Any(s => s.Name.Equals(query.RawQuery) && !s.Value.ToString()!.Contains("query"));
         }
 
         public async IAsyncEnumerable<AbstractQueryResult> Process(Query query, CancellationToken ct)
@@ -41,12 +41,12 @@ namespace Pinpoint.Plugin.Shortcuts
             ShortcutsResult result;
             if (query.Parts.Length == 1)
             {
-                result = new ShortcutsResult(query.RawQuery, UserSettings.Str(query.RawQuery));
+                result = new ShortcutsResult(query.RawQuery, Storage.UserSettings.Str(query.RawQuery));
             }
             else
             {
                 var q = query.Parts;
-                result = new ShortcutsResult(q[0], UserSettings.Str(q[0]).Replace("{query}", q[1]));
+                result = new ShortcutsResult(q[0], Storage.UserSettings.Str(q[0]).Replace("{query}", q[1]));
             }
 
             yield return result;
