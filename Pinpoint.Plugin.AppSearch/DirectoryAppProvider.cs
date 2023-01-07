b9cac69f885638d27c36ba69284f1dc3b0fc139b
Gustav <gustav-git@pm.me>
2023-01-07 11:35:26 +0100

    Provide apps from $PATH

---
+++ Pinpoint.Plugin.AppSearch/DirectoryAppProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Pinpoint.Plugin.AppSearch
+{
+    public class DirectoryAppProvider : IAppProvider
+    {
+        private readonly string[] _paths;
+
+        public DirectoryAppProvider(string[] paths)
+        {
+            _paths = paths;
+        }
+
+        public IEnumerable<IApp> Provide()
+        {
+            var uniqueAppNames = new HashSet<string>();
+            foreach (var path in _paths.Where(Directory.Exists))
+            {
+                foreach (var shortcut in FindApps(path))
+                {
+                    var nameWithoutExtension = Path.GetFileNameWithoutExtension(shortcut);
+                    if (!uniqueAppNames.Add(nameWithoutExtension))
+                    {
+                        continue;
+                    }
+
+                    yield return new StandardApp
+                    {
+                        Name = nameWithoutExtension,
+                        FilePath = shortcut,
+                        IconLocation = shortcut
+                    };
+                }
+            }
+        }
+
+        private IEnumerable<string> FindApps(string path)
+        {
+            var extensions = new[]
+            {
+                "*.appref-ms",
+                "*.lnk",
+                "*.exe"
+            };
+
+            IEnumerable<string> SafeGetFiles(string ext)
+            {
+                try
+                {
+                    return Directory.GetFiles(path, ext, SearchOption.AllDirectories);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return Array.Empty<string>();
+                }
+            }
+            
+            return extensions.SelectMany(SafeGetFiles); 
+        }
+    }
+}
---
+++ Pinpoint.Plugin.AppSearch/PathAppProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pinpoint.Plugin.AppSearch;
+
+public class PathAppProvider : IAppProvider
+{
+    private List<IApp> _cachedApps = new();
+    private string _path;
+    
+    public IEnumerable<IApp> Provide()
+    {
+        var environmentVariable = Environment.GetEnvironmentVariable("PATH");
+        if (_path == environmentVariable)
+        {
+            return _cachedApps;
+        }
+        
+        _path = environmentVariable;
+        
+        var paths = environmentVariable?.Split(';') ?? Array.Empty<string>();
+        var apps = new DirectoryAppProvider(paths).Provide();
+        _cachedApps = apps.ToList();
+        return _cachedApps;
+    }
+}
