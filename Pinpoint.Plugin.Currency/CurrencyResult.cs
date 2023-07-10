0037c221cc686e0795792c3b006ba35c9fa09f35
Gustav <gustav-git@pm.me>
2023-07-10 12:12:45 +0200

    Present currency plugin result with thousands separator (#207)

--- Pinpoint.Plugin.Currency/CurrencyResult.cs
+++ Pinpoint.Plugin.Currency/CurrencyResult.cs
@@ -6,8 +6,18 @@ namespace Pinpoint.Plugin.Currency
 {
     public class CurrencyResult : CopyabableQueryOption
     {
-        public CurrencyResult(double value, string to) : base("= " + value.ToString(CultureInfo.InvariantCulture) + " " + to.ToUpper(), value.ToString())
+        public CurrencyResult(double value, string to) : base(FormatValue(value, to), value.ToString())
         {
+        }
+
+        private static string FormatValue(double amount, string currencyToISO4217)
+        {
+            var numberFormat = CultureInfo.CurrentCulture.NumberFormat;
+            var formattedAmount = amount.ToString("N", numberFormat);
+
+            currencyToISO4217 = currencyToISO4217.ToUpper();
+
+            return $"{formattedAmount} {currencyToISO4217}";
         }
 
         public override EFontAwesomeIcon FontAwesomeIcon => EFontAwesomeIcon.Solid_Coins;
